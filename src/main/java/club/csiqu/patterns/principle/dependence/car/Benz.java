2e4da3de5fb19d4f9af9e8f7368a92bfd88b9f56
chensiqu <540498860@qq.com>
2020-03-06 16:53:04 +0800

    feat: 依赖倒置原则

---
+++ src/main/java/club/csiqu/patterns/principle/dependence/car/Benz.java
@@ -0,0 +1,13 @@
+package club.csiqu.patterns.principle.dependence.car;
+
+/**
+ * @author Siqu Chen 2020/3/5
+ * @since 1.0.0
+ */
+public class Benz implements Car {
+
+    @Override
+    public void run() {
+        System.out.println("奔驰车开动");
+    }
+}
---
+++ src/main/java/club/csiqu/patterns/principle/dependence/car/Car.java
@@ -0,0 +1,13 @@
+package club.csiqu.patterns.principle.dependence.car;
+
+/**
+ * @author Siqu Chen 2020/3/5
+ * @since 1.0.0
+ */
+public interface Car {
+
+    /**
+     * 启动
+     */
+    void run();
+}
