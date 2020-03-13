6e5869f6f0344cd58f79931023715bacf2e520dd
Discord010 <540498860@qq.com>
2020-03-13 10:01:40 +0800

    feat: leetcode(231)

---
+++ src/main/java/club/csiqu/practice/leetcode/IntegerSummary.java
@@ -0,0 +1,19 @@
+package club.csiqu.practice.leetcode;
+
+/**
+ * @author Siqu Chen 2020/3/13
+ * @since 1.0.0
+ */
+@SuppressWarnings("unused")
+public class IntegerSummary {
+
+    /**
+     * 给定整数，判断它是否是 2的幂次方
+     *
+     * @param n 整数
+     * @return 是否 2的幂次方
+     */
+    public boolean isPowerOfTwo(int n) {
+        return (n > 0) && (1 << 30) % n == 0;
+    }
+}
