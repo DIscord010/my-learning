501e2867730162491ebafe23a1e6a6c6df7641c8
Discord010 <540498860@qq.com>
2020-03-26 15:58:07 +0800

    feat: simple invert index

--- learn-structure/src/main/java/club/csiqu/learn/structure/index/InvertedIndex.java
+++ learn-structure/src/main/java/club/csiqu/learn/structure/index/InvertedIndex.java
@@ -2,7 +2,10 @@ package club.csiqu.learn.structure.index;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 倒排索引的简单实现。
@@ -26,22 +29,17 @@ public class InvertedIndex {
      * @param content  文件文本内容，即字符串。
      * @param filePath 文件路径
      */
-    private void creatIndex(String content, String filePath) {
-
+    public void creatIndex(String content, String filePath) {
         List<String> list;
-
         // 根据空格分词
         String[] words = content.split(" ");
-
         for (String word : words) {
             if (!indexMap.containsKey(word)) {
-
                 list = new ArrayList<>();
                 list.add(filePath);
                 indexMap.put(word, list);
                 count.put(word, 1);
             } else {
-
                 list = indexMap.get(word);
                 if (!list.contains(filePath)) {
                     list.add(filePath);
@@ -52,15 +50,16 @@ public class InvertedIndex {
     }
 
     /**
-     * 根据单词输出查询到的文件内容
+     * 根据单词返回文件路径
      *
-     * @param word 指定单词
+     * @param word 目标单词
+     * @return 拥有该单词的文本路径
      */
-    private void findWord(String word) {
-
+    public List<String> findWord(String word) {
         if (indexMap.containsKey(word)) {
-            List<String> list = indexMap.get(word);
-            list.forEach(s -> System.out.println(getFile(s)));
+            return indexMap.get(word);
+        } else {
+            return new ArrayList<>(0);
         }
     }
 
@@ -70,44 +69,11 @@ public class InvertedIndex {
      * @param filePath 文件路径
      * @return 文件内容字符串
      */
-    private String getFile(String filePath) {
-
+    public String getFile(String filePath) throws IOException {
         File file = new File(filePath);
-
         try (InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
              BufferedReader reader = new BufferedReader(inputStreamReader)) {
             return reader.readLine();
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-        return null;
-
-    }
-
-    public static void main(String[] args) {
-        InvertedIndex invertedIndex = new InvertedIndex();
-
-        // 根据文件名读取文件内容并建立倒排索引
-        for (int i = 1; i <= 3; i++) {
-            String path = "src/main/resources/index/"
-                    + i + ".txt";
-            invertedIndex.creatIndex(Objects.requireNonNull(invertedIndex.getFile(path)), path);
-        }
-
-        // 输出倒排索引的全部内容
-        for (Map.Entry<String, List<String>> map : invertedIndex.indexMap.entrySet()) {
-            System.out.println(map.getKey() + ":" + map.getValue());
-        }
-
-        // 输出单词统计
-        for (Map.Entry<String, Integer> num : invertedIndex.count.entrySet()) {
-            System.out.println(num.getKey() + ":" + num.getValue());
-        }
-
-        // 查询拥有单词 love的文件内容
-        invertedIndex.findWord("love");
-        // 查询拥有单词 i的文件内容
-        invertedIndex.findWord("i");
     }
 }
---
+++ learn-structure/src/test/java/club/csiqu/learn/structure/index/InvertedIndexTest.java
@@ -0,0 +1,29 @@
+package club.csiqu.learn.structure.index;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Siqu Chen 2020/3/26
+ * @since 1.0.0
+ */
+class InvertedIndexTest {
+
+    @Test
+    void testFindWord() throws IOException {
+        InvertedIndex invertedIndex = new InvertedIndex();
+        // 根据文件名读取文件内容并建立倒排索引
+        for (int i = 1; i <= 3; i++) {
+            String path = getClass().getResource("/").getPath() + "index/"
+                    + i + ".txt";
+            invertedIndex.creatIndex(Objects.requireNonNull(invertedIndex.getFile(path)), path);
+        }
+        List<String> filePaths = invertedIndex.findWord("hangzhou");
+        Assertions.assertEquals(1, filePaths.size());
+        Assertions.assertEquals("i live in hangzhou where are you.", invertedIndex.getFile(filePaths.get(0)));
+    }
+}
