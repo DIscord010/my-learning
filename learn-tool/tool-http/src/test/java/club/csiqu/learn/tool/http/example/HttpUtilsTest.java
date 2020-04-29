13a588a9d01cd1f5ec7b81d7e1190b615154c191
Discord010 <540498860@qq.com>
2020-04-29 09:11:48 +0800

    test: HttpUtils test case

--- learn-tool/tool-http/src/test/java/club/csiqu/learn/tool/http/example/HttpUtilsTest.java
+++ learn-tool/tool-http/src/test/java/club/csiqu/learn/tool/http/example/HttpUtilsTest.java
@@ -1,11 +1,13 @@
 package club.csiqu.learn.tool.http.example;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -13,6 +15,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 比较每次都新建一个 {@link HttpClient}实例对象进行发送请求（每次请求都新建并关闭连接），
@@ -26,7 +30,19 @@ public class HttpUtilsTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpUtilsTest.class);
 
-    private static final String URL = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=17705911650";
+    private static final String GET_URL = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm?tel=17705911650";
+
+    private static final String POST_URL = "https://tcc.taobao.com/cc/json/mobile_tel_segment.htm";
+
+    @Test
+    public void testHttpUtils() {
+        NameValuePair pair = new BasicNameValuePair("tel", "17705911650");
+        List<NameValuePair> params = new ArrayList<>(1);
+        params.add(pair);
+        String postResult = HttpUtils.doPost(POST_URL, null, params);
+        String getResult = HttpUtils.doGet(GET_URL, null);
+        Assertions.assertEquals(getResult, postResult);
+    }
 
     @Test
     public void testTimeCost() throws IOException {
@@ -34,7 +50,7 @@ public class HttpUtilsTest {
         long beforeTimeOffset0 = System.currentTimeMillis();
         for (int i = 0; i <= 20; i++) {
             try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                HttpGet httpGet = new HttpGet(URL);
+                HttpGet httpGet = new HttpGet(GET_URL);
                 CloseableHttpResponse response = httpClient.execute(httpGet);
                 HttpEntity entity = response.getEntity();
                 result0 = EntityUtils.toString(entity);
@@ -47,7 +63,7 @@ public class HttpUtilsTest {
         String result1 = null;
         long beforeTimeOffset1 = System.currentTimeMillis();
         for (int i = 0; i < 20; i++) {
-            result1 = HttpUtils.doGet(URL, null);
+            result1 = HttpUtils.doGet(GET_URL, null);
         }
         long afterTimeOffset1 = System.currentTimeMillis();
         long costTime1 = afterTimeOffset1 - beforeTimeOffset1;
