533b1d85e9d5124015e1ddc691cc844ab7fb83b3
Discord010 <540498860@qq.com>
2020-05-25 10:21:01 +0800

    feat: 按权重轮询负载均衡算法

---
+++ learn-algorithm/src/main/java/club/csiqu/learn/algorithm/balance/RoundRobinLoadBalance.java
@@ -0,0 +1,94 @@
+package club.csiqu.learn.algorithm.balance;
+
+import club.csiqu.learn.algorithm.balance.model.Invoker;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 带权重的轮训负载均衡算法.
+ * 例如 [A, B, C]权重为 [8, 1, 1]，则十次调用，次数比为 [8: 1: 1].
+ *
+ * @author Siqu Chen 2020/5/22
+ * @since 1.0.0
+ */
+public class RoundRobinLoadBalance extends AbstractLoadBalance {
+
+    private final Map<String, WeightedRoundRobin> weightedRoundRobinMap = new ConcurrentHashMap<>(16);
+
+    private static final int RECYCLE_PERIOD = 60000;
+
+    private static class WeightedRoundRobin {
+
+        private final AtomicLong current = new AtomicLong(0);
+
+        private int weight;
+
+        private long lastUpdate;
+
+        public long increaseCurrent() {
+            return current.addAndGet(weight);
+        }
+
+        public void reset(int total) {
+            current.addAndGet(-1L * total);
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+
+        public long getLastUpdate() {
+            return lastUpdate;
+        }
+
+        public void setLastUpdate(long lastUpdate) {
+            this.lastUpdate = lastUpdate;
+        }
+    }
+
+    @Override
+    Invoker doSelect(List<Invoker> invokers) {
+        long currentMax = Long.MIN_VALUE;
+        long now = System.currentTimeMillis();
+        int totalWeight = 0;
+        Invoker selectedInvoker = null;
+        WeightedRoundRobin selectedWeightedRoundRobin = null;
+        for (Invoker invoker : invokers) {
+            int weight = invoker.getWeight();
+            WeightedRoundRobin weightedRoundRobin = weightedRoundRobinMap.computeIfAbsent(invoker.toString(), k -> {
+                        WeightedRoundRobin wrr = new WeightedRoundRobin();
+                        wrr.setWeight(weight);
+                        return wrr;
+                    }
+            );
+            if (weight != weightedRoundRobin.getWeight()) {
+                weightedRoundRobin.setWeight(weight);
+            }
+            long current = weightedRoundRobin.increaseCurrent();
+            weightedRoundRobin.setLastUpdate(now);
+            if (current > currentMax) {
+                currentMax = current;
+                selectedInvoker = invoker;
+                selectedWeightedRoundRobin = weightedRoundRobin;
+            }
+            totalWeight += weight;
+        }
+        // 回收 30s未活动的 weightedRoundRobin
+        if (invokers.size() != weightedRoundRobinMap.size()) {
+            weightedRoundRobinMap.entrySet().removeIf(item -> now - item.getValue().getLastUpdate() > RECYCLE_PERIOD);
+        }
+        if (selectedInvoker != null) {
+            selectedWeightedRoundRobin.reset(totalWeight);
+            return selectedInvoker;
+        }
+        // 正常不会执行到这里
+        return invokers.get(0);
+    }
+}
---
+++ learn-algorithm/src/test/java/club/csiqu/learn/algorithm/balance/RoundRobinLoadBalanceTest.java
@@ -0,0 +1,40 @@
+package club.csiqu.learn.algorithm.balance;
+
+import club.csiqu.learn.algorithm.balance.model.Invoker;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Siqu Chen 2020/5/25
+ * @since 1.0.0
+ */
+class RoundRobinLoadBalanceTest {
+
+    AbstractLoadBalance roundRobinLoadBalance = new RoundRobinLoadBalance();
+
+    @Test
+    void testSelect() {
+        List<Invoker> invokers = new ArrayList<>(2);
+        Invoker invoker1 = new Invoker(80, 0);
+        Invoker invoker2 = new Invoker(10, 0);
+        Invoker invoker3 = new Invoker(10, 0);
+        invokers.add(invoker1);
+        invokers.add(invoker2);
+        invokers.add(invoker3);
+        // [A, B, C]权重为 [80, 10, 10].那么调用十次结果为：A A A B A A C A A A
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker2, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker3, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+        Assertions.assertEquals(invoker1, roundRobinLoadBalance.select(invokers));
+    }
+}
