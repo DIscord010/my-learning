997c70156b8bbf7660cfa6d27582bbd03e02f061
Discord010 <540498860@qq.com>
2020-05-28 15:56:15 +0800

    test: MyClassLoader test case

---
+++ learn-basis/src/test/java/club/csiqu/learn/basis/jvm/loading/MyClassLoaderTest.java
@@ -0,0 +1,21 @@
+package club.csiqu.learn.basis.jvm.loading;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Siqu Chen 2020/5/27
+ * @since 1.0.0
+ */
+class MyClassLoaderTest {
+
+    @Test
+    public void testLoadClass() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Object obj1 = MyClassLoader.class.newInstance();
+        // 获取自定义类加载器加载的类的实例对象
+        Object obj2 = myClassLoader.loadClass("club.csiqu.learn.basis.jvm.loading.MyClassLoader").newInstance();
+        Assertions.assertEquals(obj1.getClass().toString(), obj2.getClass().toString());
+        Assertions.assertFalse(obj2 instanceof MyClassLoader);
+    }
+}
