1f4ae136477a282da8ca179d4f5124cb17d0c998
Discord010 <540498860@qq.com>
2020-05-28 18:20:09 +0800

    feat: Spring Boot seckill service

---
+++ learn-seckill/src/main/java/club/csiqu/learnseckill/LearnSeckillApplication.java
@@ -0,0 +1,18 @@
+package club.csiqu.learnseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 高并发服务学习。
+ *
+ * @author Siqu Chen
+ */
+@SpringBootApplication
+public class LearnSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSeckillApplication.class, args);
+    }
+
+}
---
+++ learn-seckill/src/test/java/club/csiqu/learnseckill/LearnSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package club.csiqu.learnseckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
