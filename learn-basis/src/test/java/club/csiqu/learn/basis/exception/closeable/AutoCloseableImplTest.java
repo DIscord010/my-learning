e9a9047f7d38f2cd60f09a2795ac3ecb835f5b80
Discord010 <540498860@qq.com>
2020-06-03 15:17:22 +0800

    feat: 异常屏蔽现象

---
+++ learn-basis/src/test/java/club/csiqu/learn/basis/exception/closeable/AutoCloseableImplTest.java
@@ -0,0 +1,47 @@
+package club.csiqu.learn.basis.exception.closeable;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 验证 Java异常屏蔽现象
+ *
+ * @author Siqu Chen 2020/6/3
+ * @since 1.0.0
+ */
+class AutoCloseableImplTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutoCloseableImplTest.class);
+
+    @Test
+    public void testExceptionMask() {
+        // finally块抛出的异常会屏蔽 try块产生的异常
+        AutoCloseableImpl autoCloseable = new AutoCloseableImpl();
+        try {
+            try {
+                autoCloseable.sendData();
+            } finally {
+                autoCloseable.close();
+            }
+        } catch (Exception e) {
+            LOGGER.debug("异常栈：", e);
+            Assertions.assertEquals(0, e.getSuppressed().length);
+            Assertions.assertTrue(e instanceof IllegalAccessException);
+        }
+    }
+
+    @Test
+    public void testExceptionTryResource() {
+        // finally块抛出的异常会屏蔽 try块产生的异常
+        try {
+            try (AutoCloseableImpl autoCloseable = new AutoCloseableImpl()) {
+                autoCloseable.sendData();
+            }
+        } catch (Exception e) {
+            LOGGER.debug("异常栈：", e);
+            Assertions.assertEquals(1, e.getSuppressed().length);
+        }
+    }
+}
