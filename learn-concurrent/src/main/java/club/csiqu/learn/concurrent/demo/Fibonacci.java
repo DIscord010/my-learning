8f470216a7192b9e4f62847e853133d0433e3ab8
Discord010 <540498860@qq.com>
2020-07-30 09:19:02 +0800

    feat: ForkJoinPool计算斐波那契数列

---
+++ learn-concurrent/src/main/java/club/csiqu/learn/concurrent/demo/Fibonacci.java
@@ -0,0 +1,32 @@
+package club.csiqu.learn.concurrent.demo;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * 使用 {@link ForkJoinPool}计算斐波切那数列。
+ *
+ * @author chensiqu [540498860@qq.com]
+ * @since 2019/8/23 16:58
+ */
+public class Fibonacci extends RecursiveTask<Long> {
+
+    private static final long serialVersionUID = 5430812783843847303L;
+
+    final long n;
+
+    Fibonacci(long n) {
+        this.n = n;
+    }
+
+    @Override
+    protected Long compute() {
+        if (n <= 1) {
+            return n;
+        }
+        Fibonacci f1 = new Fibonacci(n - 1);
+        f1.fork();
+        Fibonacci f2 = new Fibonacci(n - 2);
+        return f2.compute() + f1.join();
+    }
+}
---
+++ learn-concurrent/src/test/java/club/csiqu/learn/concurrent/demo/FibonacciTest.java
@@ -0,0 +1,20 @@
+package club.csiqu.learn.concurrent.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author Siqu Chen 2020/7/30
+ * @since 1.0
+ */
+class FibonacciTest {
+
+    @Test
+    void testFibonacci() {
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        Long result = forkJoinPool.invoke(new Fibonacci(5));
+        Assertions.assertEquals(5, result);
+    }
+}
