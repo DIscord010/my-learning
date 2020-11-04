637305de0ac5961eeb5dcf27ab0726578a4c8626
DIscord010 <540498860@qq.com>
2020-11-04 18:35:55 +0800

    feat: SmartInitializingSingleton

--- learn-framework/learn-spring/src/main/java/club/csiqu/learn/framework/spring/ioc/example/config/SpringConfig.java
+++ learn-framework/learn-spring/src/main/java/club/csiqu/learn/framework/spring/ioc/example/config/SpringConfig.java
@@ -3,6 +3,9 @@ package club.csiqu.learn.framework.spring.ioc.example.config;
 import club.csiqu.learn.framework.spring.ioc.example.service.HelloWorldService;
 import club.csiqu.learn.framework.spring.ioc.example.service.HelloWorld;
 import club.csiqu.learn.framework.spring.ioc.example.service.impl.AutumnHelloWorldImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.SmartInitializingSingleton;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +15,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(SpringConfig.class);
+
     @Bean
     public HelloWorld autumnHelloWorldImpl() {
         return new AutumnHelloWorldImpl();
@@ -20,5 +25,10 @@ public class SpringConfig {
     @Bean
     public HelloWorldService helloWorldService() {
         return new HelloWorldService(autumnHelloWorldImpl());
+    }
+
+    @Bean
+    public SmartInitializingSingleton smartInitializingSingleton() {
+        return () -> LOGGER.info("All Singleton bean Instantiated.");
     }
 }
